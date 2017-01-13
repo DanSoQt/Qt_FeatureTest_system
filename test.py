2d86a39cd69967b89a31e0ad100a3b0170d52bc2
Paul Olav Tvete <paul.tvete@qt.io>
2017-01-13 11:42:17 +0100

    Major update

--- test.py
+++ test.py
@@ -1,201 +1,199 @@
 import subprocess
 import datetime
 
-features_qml = [
-    'qml-interpreter',
-    'qml-network',
-]
-features_quick = [
-    'd3d12',
-    'quick-animatedimage',
-    'quick-canvas',
-    'quick-designer',
-    'quick-flipable',
-    'quick-gridview',
-    'quick-listview',
-    'quick-path',
-    'quick-pathview',
-    'quick-positioners',
-    'quick-shadereffect',
-    'quick-sprite',
-]
-features_xmlpatterns = [
-    'xml-schema',
-]
-features_widgets = [
-    'buttongroup',
-    'calendarwidget',
-    'colordialog',
-    'columnview',
-    'combobox',
-    'completer',
-    'contextmenu',
-    'datawidgetmapper',
-    'datetimeedit',
-    'dial',
-    'dirmodel',
-    'dockwidget',
-    'effects',
-    'errormessage',
-    'filedialog',
-    'filesystemmodel',
-    'fontcombobox',
-    'fontdialog',
-    'fscompleter',
-    'graphicseffect',
-    'graphicsview',
-    'groupbox',
-    'inputdialog',
-    'itemviews',
-    'keysequenceedit',
-    'lcdnumber',
-    'lineedit',
-    'listview',
-    'listwidget',
-    'mainwindow',
-    'mdiarea',
-    'menu',
-    'menubar',
-    'messagebox',
-    'paint_debug',
-    'progressbar',
-    'progressdialog',
-    'resizehandler',
-    'rubberband',
-    'scrollarea',
-    'scrollbar',
-    'sizegrip',
-    'slider',
-    'spinbox',
-    'splashscreen',
-    'splitter',
-    'stackedwidget',
-    'statusbar',
-    'statustip',
-    'style-stylesheet',
-    'syntaxhighlighter',
-    'tabbar',
-    'tableview',
-    'tablewidget',
-    'tabwidget',
-    'textbrowser',
-    'textedit',
-    'toolbar',
-    'toolbox',
-    'toolbutton',
-    'tooltip',
-    'treeview',
-    'treewidget',
-    'undocommand',
-    'undogroup',
-    'undostack',
-    'undoview',
-    'whatsthis',
-    'wizard',
-]
-features_network = [
-    'bearermanagement',
-    'ftp',
-    'http',
-    'localserver',
-    'networkdiskcache',
-    'networkinterface',
-    'networkproxy',
-    'socks5',
-    'udpsocket',
-]
-features_printsupport = [
-    'cups',
-    'printdialog',
-    'printer',
-    'printpreviewdialog',
-    'printpreviewwidget',
-]
-features_core = [
-    'animation',
-    'big_codecs',
-    'codecs',
-    'commandlineparser',
-    'datestring',
-    'filesystemiterator',
-    'filesystemwatcher',
-    'gestures',
-    'iconv',
-    'identityproxymodel',
-    'itemmodel',
-    'library',
-    'mimetype',
-    'process',
-    'properties',
-    'proxymodel',
-    'regularexpression',
-    'settings',
-    'sha3-fast',
-    'sharedmemory',
-    'sortfilterproxymodel',
-    'statemachine',
-    'stringlistmodel',
-    'systemsemaphore',
-    'temporaryfile',
-    'textcodec',
-    'textdate',
-    'timezone',
-    'topleveldomain',
-    'translation',
-    'xmlstream',
-    'xmlstreamreader',
-    'xmlstreamwriter',
-]
-features_gui = [
-    'accessibility',
-    'action',
-    'clipboard',
-    'colornames',
-    'cssparser',
-    'cursor',
-    'desktopservices',
-    'draganddrop',
-    'freetype',
-    'highdpiscaling',
-    'im',
-    'image_heuristic_mask',
-    'image_text',
-    'imageformat_bmp',
-    'imageformat_jpeg',
-    'imageformat_png',
-    'imageformat_ppm',
-    'imageformat_xbm',
-    'imageformat_xpm',
-    'imageformatplugin',
-    'movie',
-    'pdf',
-    'picture',
-    'sessionmanager',
-    'shortcut',
-    'standarditemmodel',
-    'systemtrayicon',
-    'tabletevent',
-    'texthtmlparser',
-    'textodfwriter',
-    'validator',
-    'wheelevent',
-]
-features_xml = [
-    'dom',
-]
+repo_features = dict()
 
-features = features_core.copy()
-features.extend(features_gui)
-features.extend(features_qml)
-features.extend(features_quick)
-features.extend(features_xmlpatterns)
-features.extend(features_network)
-features.extend(features_printsupport)
-features.extend(features_xml)
+features_qml = (
+    '-no-feature-qml-interpreter',
+    '-no-feature-qml-network',
+)
+features_quick = (
+    '-no-feature-d3d12',
+    '-no-feature-quick-animatedimage',
+    '-no-feature-quick-canvas',
+    '-no-feature-quick-designer',
+    '-no-feature-quick-flipable',
+    '-no-feature-quick-gridview',
+    '-no-feature-quick-listview',
+    '-no-feature-quick-path',
+    '-no-feature-quick-pathview',
+    '-no-feature-quick-positioners',
+    '-no-feature-quick-shadereffect',
+    '-no-feature-quick-sprite',
+)
 
-#features.extend(features_widgets)
+repo_features['qtdeclarative'] = features_qml + features_quick
 
-components = [
+features_xmlpatterns = (
+    '-no-feature-xml-schema',
+)
+
+repo_features['qtxmlpatterns'] = features_xmlpatterns
+
+features_widgets = (
+    '-no-feature-buttongroup',
+    '-no-feature-calendarwidget',
+    '-no-feature-colordialog',
+    '-no-feature-columnview',
+    '-no-feature-combobox',
+    '-no-feature-completer',
+    '-no-feature-contextmenu',
+    '-no-feature-datawidgetmapper',
+    '-no-feature-datetimeedit',
+    '-no-feature-dial',
+    '-no-feature-dirmodel',
+    '-no-feature-dockwidget',
+    '-no-feature-effects',
+    '-no-feature-errormessage',
+    '-no-feature-filedialog',
+    '-no-feature-filesystemmodel',
+    '-no-feature-fontcombobox',
+    '-no-feature-fontdialog',
+    '-no-feature-fscompleter',
+    '-no-feature-graphicseffect',
+    '-no-feature-graphicsview',
+    '-no-feature-groupbox',
+    '-no-feature-inputdialog',
+    '-no-feature-itemviews',
+    '-no-feature-keysequenceedit',
+    '-no-feature-lcdnumber',
+    '-no-feature-lineedit',
+    '-no-feature-listview',
+    '-no-feature-listwidget',
+    '-no-feature-mainwindow',
+    '-no-feature-mdiarea',
+    '-no-feature-menu',
+    '-no-feature-menubar',
+    '-no-feature-messagebox',
+    '-no-feature-paint_debug',
+    '-no-feature-progressbar',
+    '-no-feature-progressdialog',
+    '-no-feature-resizehandler',
+    '-no-feature-rubberband',
+    '-no-feature-scrollarea',
+    '-no-feature-scrollbar',
+    '-no-feature-sizegrip',
+    '-no-feature-slider',
+    '-no-feature-spinbox',
+    '-no-feature-splashscreen',
+    '-no-feature-splitter',
+    '-no-feature-stackedwidget',
+    '-no-feature-statusbar',
+    '-no-feature-statustip',
+    '-no-feature-style-stylesheet',
+    '-no-feature-syntaxhighlighter',
+    '-no-feature-tabbar',
+    '-no-feature-tableview',
+    '-no-feature-tablewidget',
+    '-no-feature-tabwidget',
+    '-no-feature-textbrowser',
+    '-no-feature-textedit',
+    '-no-feature-toolbar',
+    '-no-feature-toolbox',
+    '-no-feature-toolbutton',
+    '-no-feature-tooltip',
+    '-no-feature-treeview',
+    '-no-feature-treewidget',
+    '-no-feature-undocommand',
+    '-no-feature-undogroup',
+    '-no-feature-undostack',
+    '-no-feature-undoview',
+    '-no-feature-whatsthis',
+    '-no-feature-wizard',
+)
+features_network = (
+    '-no-feature-bearermanagement',
+    '-no-feature-ftp',
+    '-no-feature-http',
+    '-no-feature-localserver',
+    '-no-feature-networkdiskcache',
+    '-no-feature-networkinterface',
+    '-no-feature-networkproxy',
+    '-no-feature-socks5',
+    '-no-feature-udpsocket',
+)
+features_printsupport = (
+    '-no-feature-cups',
+    '-no-feature-printdialog',
+    '-no-feature-printer',
+    '-no-feature-printpreviewdialog',
+    '-no-feature-printpreviewwidget',
+)
+features_core = (
+    '-no-feature-animation',
+    '-no-feature-big_codecs',
+    '-no-feature-codecs',
+    '-no-feature-commandlineparser',
+    '-no-feature-datestring',
+    '-no-feature-filesystemiterator',
+    '-no-feature-filesystemwatcher',
+    '-no-feature-gestures',
+    '-no-feature-iconv',
+    '-no-feature-identityproxymodel',
+    '-no-feature-itemmodel',
+    '-no-feature-library',
+    '-no-feature-mimetype',
+    '-no-feature-process',
+    '-no-feature-properties',
+    '-no-feature-proxymodel',
+    '-no-feature-regularexpression',
+    '-no-feature-settings',
+    '-no-feature-sha3-fast',
+    '-no-feature-sharedmemory',
+    '-no-feature-sortfilterproxymodel',
+    '-no-feature-statemachine',
+    '-no-feature-stringlistmodel',
+    '-no-feature-systemsemaphore',
+    '-no-feature-temporaryfile',
+    '-no-feature-textcodec',
+    '-no-feature-textdate',
+    '-no-feature-timezone',
+    '-no-feature-topleveldomain',
+    '-no-feature-translation',
+    '-no-feature-xmlstream',
+    '-no-feature-xmlstreamreader',
+    '-no-feature-xmlstreamwriter',
+)
+features_gui = (
+    '-no-feature-accessibility',
+    '-no-feature-action',
+    '-no-feature-clipboard',
+    '-no-feature-colornames',
+    '-no-feature-cssparser',
+    '-no-feature-cursor',
+    '-no-feature-desktopservices',
+    '-no-feature-draganddrop',
+    '-no-feature-freetype',
+    '-no-feature-highdpiscaling',
+    '-no-feature-im',
+    '-no-feature-image_heuristic_mask',
+    '-no-feature-image_text',
+    '-no-feature-imageformat_bmp',
+    '-no-feature-imageformat_jpeg',
+    '-no-feature-imageformat_png',
+    '-no-feature-imageformat_ppm',
+    '-no-feature-imageformat_xbm',
+    '-no-feature-imageformat_xpm',
+    '-no-feature-imageformatplugin',
+    '-no-feature-movie',
+    '-no-feature-pdf',
+    '-no-feature-picture',
+    '-no-feature-sessionmanager',
+    '-no-feature-shortcut',
+    '-no-feature-standarditemmodel',
+    '-no-feature-systemtrayicon',
+    '-no-feature-tabletevent',
+    '-no-feature-texthtmlparser',
+    '-no-feature-textodfwriter',
+    '-no-feature-validator',
+    '-no-feature-wheelevent',
+)
+features_xml = (
+    '-no-feature-dom',
+)
+
+components_qtbase = (
+    '__baseline__',
     '-no-widgets',
     '-no-dbus',
     '-no-gui',
@@ -206,7 +204,15 @@ components = [
     '-no-freetype',
     '-no-harfbuzz',
     '-no-ssl',
-]
+    '-no-xkbcommon'
+)
+
+repo_features['qtbase'] = components_qtbase
+
+repo_features['qtbase'] += features_network + features_printsupport + features_core + features_gui + features_xml
+
+### repo_features['qtbase'] += features_widgets
+
 
 repos = [
     'qtbase',
@@ -221,16 +227,16 @@ repos = [
     'qtcharts',
     'qtconnectivity',
     'qtdatavis3d',
-    'qtdoc',
+#    'qtdoc',
     'qtgamepad',
     'qtgraphicaleffects',
     'qtimageformats',
     'qtlocation',
     'qtmacextras',
     'qtmultimedia',
-    'qtnetworkauth',
+####    'qtnetworkauth',
     'qtpurchasing',
-    'qtqa',
+#    'qtqa',
     'qtquickcontrols2',
 #    'qtrepotools',
 #    'qtscript',
@@ -251,55 +257,162 @@ repos = [
 ]
 
 
-#./configure -recheck-all -no-pch -release -developer-build -no-warnings-are-errors -nomake examples -nomake tests -opensource -confirm-license -no-feature-wheelevent
+
+deps = dict()
+deps["qtdeclarative"] = ('qtxmlpatterns',)
+
+deps["qtmultimedia"] = ('qtdeclarative',)
+deps["qtlocation"] = ('qtdeclarative', 'qtquickcontrols', 'qtserialport',)
+deps["qtsensors"] = ('qtdeclarative', 'qtsvg',)
+####deps["qtsystems"] = ('qtdeclarative',)
+####deps["qtfeedback"] = ('qtdeclarative', 'qtmultimedia',)
+####deps["qtpim"] = ('qtdeclarative',)
+deps["qtconnectivity"] = ('qtdeclarative', 'qtandroidextras',)
+deps["qtwayland"] = ('qtdeclarative',)
+deps["qt3d"] = ('qtdeclarative', 'qtimageformats', 'qtgamepad',)
+deps["qtgraphicaleffects"] = ('qtdeclarative',)
+deps["qtquickcontrols"] = ('qtdeclarative', 'qtgraphicaleffects',)
+deps["qtserialbus"] = ('qtserialport',)
+deps["qtwinextras"] = ('qtdeclarative', 'qtmultimedia',)
+deps["qtcanvas3d"] = ('qtdeclarative',)
+deps["qtquickcontrols2"] = ('qtgraphicaleffects',)
+deps["qtpurchasing"] = ('qtandroidextras', 'qtdeclarative',)
+deps["qtcharts"] = ('qtdeclarative', 'qtmultimedia',)
+deps["qtdatavis3d"] = ('qtdeclarative', 'qtmultimedia',)
+deps["qtvirtualkeyboard"] = ('qtdeclarative', 'qtsvg', 'qtmultimedia', 'qtquickcontrols',)
+deps["qtgamepad"] = ('qtdeclarative',)
+deps["qtscxml"] = ('qtdeclarative',)
+deps["qtspeech"] = ('qtdeclarative', 'qtmultimedia',)
+####deps["qtnetworkauth"] = ('qtwebview',)
 
 
-def timestamp():
-    return '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+def deps_recursive(repo):
+    retval = set()
+    if not repo in deps:
+        return retval
+    for r in deps[repo]:
+        retval.add(r)
+        retval |= deps_recursive(r)
+    return retval
+    
 
+rrdeps = {x:{x} for x in repos}
 
+#print("one", rrdeps)
+
+for r in repos:
+    dr = deps_recursive(r)
+    #print("deps of", r, "=", dr)
+    for rr in dr:
+        rrdeps[rr].add(r)
+
+#for key, value in deps.items():
+#    for d in value:
+#        print("dep", key, "is", d)
+#        rdeps[d].append(key)
+#
+#
+#rdeps['qtbase'] = repos.copy()
+#rdeps['qtbase'].remove('qtbase')
+#
+
+rrdeps['qtbase'] = set(repos)
+
+print(rrdeps)
 
 configuretemplate = [ "./configure", "-recheck-all", "-no-pch", "-release", "-developer-build", "-no-warnings-are-errors", "-nomake", "examples", "-nomake", "tests", "-opensource", "-confirm-license" ]
 
 outfile = open('results.txt', 'w')
 errfile = open('errors.txt', 'w')
 
-options = [ '__baseline__' ]
 
-options.extend(components)
+#./configure -recheck-all -no-pch -release -developer-build -no-warnings-are-errors -nomake examples -nomake tests -opensource -confirm-license -no-feature-wheelevent
 
-for ff in features:
-    options.append("-no-feature-"+ff)
+def timestamp():
+    return '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
 
-for opt in options:
-    print("cleaning")
-    subprocess.run(["git", "submodule", "foreach", "git", "clean", "-fdx"])
+
+#def allrdeps(repo):
+#    res = set(rdeps[repo])
+#    for r in rdeps[repo]:
+#        res |= allrdeps(r)
+#    res.add(repo)
+#    return res
+#
+
+def clean_repos(repos_to_clean):
+    for r in repos_to_clean:
+        print("cleaning", r)
+        subprocess.run(["git", "clean", "-fdx"], cwd=r, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+def configure_qt(opt):
     print(timestamp(), "Configuring", opt, file=outfile, flush=True)
     configurecmd = configuretemplate.copy()
     if opt != '__baseline__':
         configurecmd.append(opt)
 
     print("configuring", opt)
-    conf_retc = subprocess.run(configurecmd, check=False)
+    ### TODO: capture stderr
+    conf_retc = subprocess.run(configurecmd, check=False, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     print(timestamp(), 'Configure result', conf_retc.returncode,  file=outfile, flush=True)
     if conf_retc.returncode != 0:
         print("Configure error:", opt, file=errfile, flush=True)
+    return conf_retc.returncode == 0
+
+
+
+def print_errors(log):
+    print(log)
+    for line in log.splitlines():
+        if ': error:' in line or 'Project ERROR:' in line or line.startswith('make['):
+            print('        ', line, file=errfile, flush=True)
+    print('----------------------------------------------------------\n')
+
+
+skip_list = []
+
+#### testing
+####skip_list = ["qtbase", "qtxmlpatterns"]
+
+
+# start out clean
+print("cleaning all")
+subprocess.run(["git", "submodule", "foreach", "git", "clean", "-fdx"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+# test all features connected to each repo
+
+for current_repo in repos:
+    print('hello', current_repo)
+    if not current_repo in repo_features:
         continue
-    for r in repos:
-        print(timestamp(), "Building",
-              r, "with", opt, file=outfile, flush=True)
-        build_retc = subprocess.run(["make", "-s", "module-" + r])
-        print(timestamp(), 'Build result',
-              build_retc.returncode, file=outfile, flush=True)
-        if build_retc.returncode != 0:
-            print(timestamp(), 'Build error',
-                  r, opt, file=errfile, flush=True)
-            if r == 'qtbase':
-                break
+
+
+    if current_repo in skip_list:
+        continue
+
+    repos_to_test = rrdeps[current_repo]
+
+    for test_feature in repo_features[current_repo]:
+        clean_repos(repos_to_test)
+        r_to_test = repos_to_test.copy()
+
+        configure_qt(test_feature)
+        for r in repos:
+            if r in r_to_test:
+                print(timestamp(), "Building", r, "with", test_feature, file=outfile, flush=True)
+                print("Building", r, "...")
+                build_retc = subprocess.run(["make", "-s", "module-" + r],
+                                            stdout=subprocess.DEVNULL, stderr=subprocess.PIPE, universal_newlines=True)
+                print(timestamp(), 'Build result',
+                      build_retc.returncode, file=outfile, flush=True)
+                success = build_retc.returncode == 0
+                if not success:
+                    print(timestamp(), 'Build error', r, test_feature, "(from", current_repo, ")", file=errfile, flush=True)
+                    r_to_test -= rrdeps[r]
+                    print_errors(build_retc.stderr)
 
 
 
 
-
-
-##result = subprocess.run(["ls", "-l", "."], stdout=subprocess.PIPE)
+    #clean all rdeps before testing next repo
+    clean_repos(repos_to_test)
